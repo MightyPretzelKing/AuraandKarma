99fae4ace22812ef5969dfc34e939b43903dd9d2
MightyPretzelKing <bradyhottmann@gmail.com>
2023-09-01 19:44:00 -0700

    Updated Item textures

--- src/main/java/net/pretzel/aura_and_karma/block/ModBlocks.java
+++ src/main/java/net/pretzel/aura_and_karma/block/ModBlocks.java
@@ -14,6 +14,7 @@ import net.minecraft.item.Item;
 public class ModBlocks {
     public static final Block SUNPLATE_BLOCK = registerBlock("sunplate_block",
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
+    //hmmm
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, new Identifier(AuraAndKarma.MOD_ID, name), block);
